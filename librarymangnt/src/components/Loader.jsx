1923fa902eb141d2d7bdb682972e4211726b2d18
prabhas-code <prabhasmadipadiga@gmail.com>
2025-11-25 12:07:40 +0530

    updated loader in frontend

--- librarymangnt/src/components/Loader.jsx
+++ librarymangnt/src/components/Loader.jsx
@@ -1,9 +1,8 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center py-20">
-      <div className="w-10 h-10 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
+    <div className="fixed inset-0 z-50 bg-white flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
-
 export default Loader;
