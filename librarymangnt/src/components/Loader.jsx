8b230d14d9ce2dd6bc6b4f47834b4d14d80d6d67
prabhas-code <prabhasmadipadiga@gmail.com>
2025-11-25 12:31:37 +0530

    updated loader

--- librarymangnt/src/components/Loader.jsx
+++ librarymangnt/src/components/Loader.jsx
@@ -1,8 +1,9 @@
 const Loader = () => {
   return (
-    <div className="fixed inset-0 z-50 bg-white flex justify-center items-center">
+    <div className="fixed inset-0 z-[9999] bg-white flex justify-center items-center">
       <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
+
 export default Loader;
